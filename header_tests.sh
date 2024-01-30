6c31b6dc2ffe7cab350ea393cdf11fa3c91067ae
Eric LOQUET <eric.loquet@capgemini.com>
2024-01-30 17:54:42 +0100

    new version using pytest instead of deprecated nose
    Signed-off-by: Eric LOQUET <eric.loquet@capgemini.com>

--- header_tests.sh
+++ header_tests.sh
@@ -5,29 +5,30 @@ launch_tests() {
    echo $1 testing...
    #test-file-pattern = l1s_test_header.py
    #test-file-pattern = l*_test*.py
-   sed -i -E "s/^test-file-pattern = .{1,}py$/test-file-pattern = l1s_test_header.py/g" nose2.cfg
+   #sed -i -E "s/^test-file-pattern = .{1,}py$/test-file-pattern = l1s_test_header.py/g" nose2.cfg
+   #nose2 
 
-   nose2 
+   pytest -v ./$2/tests/l1s_test_header.py::Testheader
 }
 
 cd ./models/witness-core
-launch_tests witness-core
+launch_tests witness-core climateeconomics
 
 cd -
 cd ./models/witness-energy
-launch_tests witness-energy
+launch_tests witness-energy energy_models
 
 cd -
-cd ./platform/sostrades-core
-launch_tests sostrades-core
+cd ./platform/sostrades-core 
+launch_tests sostrades-core sostrades_core
 
 cd -
-cd ./platform/sostrades-ontology
-launch_tests sostrades-ontology
+cd ./platform/sostrades-ontology 
+launch_tests sostrades-ontology sos_ontology
 
 cd -
-cd ./platform/sostrades-webapi
-launch_tests sostrades-webapi
+cd ./platform/sostrades-webapi 
+launch_tests sostrades-webapi sos_trades_api
 
 
 
