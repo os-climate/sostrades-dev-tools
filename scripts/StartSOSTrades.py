0f1a16b776531b43b76011e391f2f41a9e42c1d1
jlivatthana <145337844+jlivatthana@users.noreply.github.com>
2024-04-08 08:55:06 +0200

    remove useless import

--- scripts/StartSOSTrades.py
+++ scripts/StartSOSTrades.py
@@ -12,9 +12,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 '''
-
-import json
-import subprocess
 import os
 
 # Variable with the path of sostrade-dev-tools
