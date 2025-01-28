1cbf9edf2428a2d77a8105b7c133064139016131
jlivatthana <145337844+jlivatthana@users.noreply.github.com>
2025-01-28 13:54:02 +0100

    Use same command fort linux and windows environment for UpdateOntology.py script

--- scripts/UpdateOntology.py
+++ scripts/UpdateOntology.py
@@ -2,25 +2,21 @@ import os
 
 from constants import venv_script_activate_path, venv_script_activate_command, sostrades_dev_tools_path, platform_path, model_path
 from tooling import run_command, list_directory_paths
-import platform
 
+# Get all the directories in the platform and model directories
 all_platform_directory = list_directory_paths(platform_path)
 all_model_directory = list_directory_paths(model_path)
 
+# Create Pythonpath with all the models and platform depends on the system environment
 python_path=""
 for source in all_model_directory:
-    if platform.system() == "Windows":
-       python_path+= source+ os.pathsep
-    else:
-       python_path+= "'" +source + "'" + os.pathsep 
+    python_path+= source + os.pathsep
 for source in all_platform_directory:
-    if platform.system() == "Windows":
-        python_path+= source + os.pathsep
-    else:
-        python_path+= "'" +source + "'" + os.pathsep
+    python_path+= source + os.pathsep
 
-
-print(f'PYTHONPATH={python_path}')
+# Set PYTHONPATH environment variable with the path of all the models and platform
+os.environ['PYTHONPATH']= f'{python_path}'
+print(f"PYTHONPATH={os.environ.get('PYTHONPATH')}")
 
 if not os.path.exists(venv_script_activate_path):
     raise Exception("Virtual environment (.venv) is not installed")
@@ -31,14 +27,8 @@ print("Updating ontology ...")
 # Change directory to sostrades-dev-tools/platform/sostrades-ontology
 os.chdir(f"{platform_path}/sostrades-ontology")
 # Start sostrades-ontology with .venv
-if platform.system() == "Windows":
-    run_command(
-        f"{venv_script_activate_command} && set PYTHONPATH={python_path} && python sos_ontology/core/script/createSoSOntologyFromCode.py"
-    )
-else:  # For Linux
-    run_command(
-        f"{venv_script_activate_command} && export PYTHONPATH={python_path} && python sos_ontology/core/script/createSoSOntologyFromCode.py"
-    )
-print("Finished")
+run_command(
+    f'{venv_script_activate_command} && python sos_ontology/core/script/createSoSOntologyFromCode.py'
+)
 
 os.chdir(sostrades_dev_tools_path)
